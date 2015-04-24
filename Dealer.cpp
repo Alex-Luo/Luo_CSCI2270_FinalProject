9442d8ea578a1813fba3c8f943abfabacb4712c2
Brian Ensor <briancensor@gmail.com>
2015-04-23 22:17:56 -0600

    Fix Dealer.cpp

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,23 +1,40 @@
 #include "Dealer.h"
 
-void Dealer::Hit(Hand *hand){
+Dealer::Dealer(Hand *hand) {
+    _hand = new Hand;
+    hit(hand);
+    hit(_hand);
+    hit(hand);
+    hit(_hand);
+}
+
+Dealer::~Dealer() {
+    delete _hand;
+}
+
+void Dealer::hit(Hand *hand) {
 	//Deal 1 card
 	hand->addCard(_decks.pop());
 }
 
-void Dealer::Stay(){
+void Dealer::stay() {
 	//Do nothing, pass to house.
-	HouseRules();
+	houseRules();
 }
 
-void Dealer::DoubleDown(Hand *hand){
+void Dealer::doubleDown(Hand *hand) {
 	//Hit once, then has to stay.
-	Hit(hand);
-	Stay();
+	hit(hand);
+	stay();
 }
+
+Hand Dealer::getHand() {
+    return *_hand;
+}
+
  //Typical BlackJack Rules. If dealer hand is less than 17, dealer must hit.
-void Dealer::HouseRules(){
-	while(_hand->getValue() < 17){
-		Hit(_hand);
+void Dealer::houseRules() {
+	while(_hand->getValue(false) < 17){
+		hit(_hand);
 	}
 }
