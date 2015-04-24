6dfbbdd54fa44299fb668b0c50f4222d30a27bb4
luoalex222 <allu6345@colorado.edu>
2015-04-23 22:28:02 -0600

    Added comments

--- Decks.cpp
+++ Decks.cpp
@@ -2,7 +2,7 @@
 
 #include <algorithm>
 #include <random>
-
+//build the deck
 Decks::Decks() {
     for (int i = 0; i < 5; i++) {
         for (int value = 1; value <= 13; value++) {
@@ -12,20 +12,21 @@ Decks::Decks() {
             }
         }
     }
+    //shuffle the deck
     shuffle();
 }
-
+//shuffle function, calls upon built in algorithm and random functions
 void Decks::shuffle() {
     std::mt19937 r{std::random_device{}()};
     std::shuffle(_cards.begin(), _cards.end(), r);
 }
-
+//take out a card
 Card Decks::pop() {
     Card poppedCard = _cards.front();
     _cards.erase(_cards.begin());
     return poppedCard;
 }
-
+//keep track of number of cards in deck to figure out when to reshuffle
 int Decks::numCards() {
     return _cards.size();
 }
